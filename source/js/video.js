b447556307ead46b6be54e57943b3641b68c53d5
HurricaneLily <jostin27022023@gmail.com>
2024-07-06 17:57:16 +0400

    Стили (hero, price, about)

--- source/js/video.js
+++ source/js/video.js
@@ -7,8 +7,8 @@ const loadVideo = () => {
   iframe.title = 'Зал фитнес центра SUPERGYM';
   iframe.src = 'https://www.youtube.com/embed/9TZXsZItgdw?si=qRE-hAmTqfeYkKZA&autoplay=1';
   iframe.loading = 'lazy';
-  iframe.width = '320';
-  iframe.height = '170';
+  iframe.width = '100%';
+  iframe.height = '100%';
   iframe.frameborder = '0';
   iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
   iframe.referrerpolicy = 'strict-origin-when-cross-origin';
